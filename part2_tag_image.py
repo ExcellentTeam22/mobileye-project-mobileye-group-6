c999ec47c84300edac73207054a219db25f6e2b3
liron_farzam <lironfa@edu.hac.ac.il>
2022-08-04 15:49:43 +0300

    all fixed working

--- part2_tag_image.py
+++ part2_tag_image.py
@@ -9,9 +9,7 @@ import numpy as np
 
 src_img_dir = 'test2'
 des_imag_dir = 'test2_tag_image'
-img = ''
-img_name = ''
-new_name = ''
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -35,69 +33,79 @@ class bcolors:
     Default = '\033[99m'
 
 
-class Index:
+class Index(object):
+    def __init__(self, source, dest):
+        self.source= source
+        self.dest =dest
+        self.img = ''
+        self.img_name = ''
+        self.new_name = ''
+
+
+    def classify(self):
+
+        for self.filename in glob.glob(f'{self.source}/*.png'):  # assuming gif
+            print(self.filename)
+            self.img = Image.open(self.filename)
+            self.img_name = self.filename
+            f, axarr = plt.subplots(1, 1, sharex=True, sharey=True)
+            axarr.title.set_text('image')
+            axarr.imshow(self.img)
+
+
+            ax_ign = plt.axes([0.12, 0.05, 0.1, 0.075])
+            ax_not = plt.axes([0.59, 0.05, 0.1, 0.075])
+            ax_red = plt.axes([0.7, 0.05, 0.1, 0.075])
+            ax_green = plt.axes([0.81, 0.05, 0.1, 0.075])
+
+            b_not = Button(ax_not, 'Not TL', color='b')
+            b_not.on_clicked(self.b_not)
+
+            b_ign = Button(ax_ign, 'Ignore', color='w')
+            b_ign.on_clicked(self.b_ign)
+
+            b_red = Button(ax_red, 'Red', color='r')
+            b_red.on_clicked(self.b_red)
+
+            b_green = Button(ax_green, 'Green', color='g')
+            b_green.on_clicked(self.b_green)
+
+            plt.show()
+            self.img_name = ''
+            self.new_name = ''
 
     def b_green(self, event):
-        img_name2 = img_name.replace(src_img_dir, des_imag_dir)
-        new_name = img_name2.replace(".png", "_G.png")
-        plt.imsave(new_name, img)
-        print(f"{bcolors.OKGREEN}{new_name}{bcolors.ENDC}")
-        plt.close()
+        img_name2 = self.img_name.replace(src_img_dir, des_imag_dir)
+        self.new_name = img_name2.replace(".png", "_G.png")
+        self.save_image(self.new_name)
+        print(f"{bcolors.Green}{self.new_name}{bcolors.ENDC}")
 
 
     def b_red(self, event):
-        img_name2 = img_name.replace(src_img_dir,des_imag_dir)
-        new_name = img_name2.replace(".png", "_R.png")
-        plt.imsave(new_name,img)
-        print(f"{bcolors.Red}{new_name}{bcolors.ENDC}")
-        plt.close()
+        img_name2 = self.img_name.replace(self.source,self.dest)
+        self.new_name = img_name2.replace(".png", "_R.png")
+        self.save_image(self.new_name)
+        print(f"{bcolors.Red}{self.new_name}{bcolors.ENDC}")
 
 
     def b_not(self, event):
-        img_name2 = img_name.replace(src_img_dir, des_imag_dir)
-        new_name = img_name2.replace(".png", "_N.png")
-        plt.imsave(new_name, img)
-        print(f"{bcolors.Blue}{new_name}{bcolors.ENDC}")
-        plt.close()
+        img_name2 = self.img_name.replace(self.source,self.dest)
+        self.new_name = img_name2.replace(".png", "_N.png")
+        self.save_image(self.new_name)
+        print(f"{bcolors.Blue}{self.new_name}{bcolors.ENDC}")
 
 
     def b_ign(self, event):
-        img_name2 = img_name.replace(src_img_dir, des_imag_dir)
-        new_name = img_name2.replace(".png", "_I.png")
-        plt.imsave(new_name, img)
-        print(f"{bcolors.Black}{new_name}{bcolors.ENDC}")
+        img_name2 = self.img_name.replace(self.source,self.dest)
+        self.new_name = img_name2.replace(".png", "_I.png")
+        self.save_image(self.new_name)
+        print(f"{bcolors.Black}{self.new_name}{bcolors.ENDC}")
+
+
+    def save_image(self,name):
+        plt.imsave(self.new_name, np.array(self.img))
         plt.close()
 
-
 if __name__ == '__main__':
-
-    image_list = []
-    for filename in glob.glob(f'{src_img_dir}/*.png'):  # assuming gif
-        print(filename)
-        img = Image.open(filename)
-        img_name = filename
-        f, axarr = plt.subplots(1, 1, sharex=True, sharey=True)
-        axarr.title.set_text('image')
-        axarr.imshow(img)
-
-        callback = Index()
-        ax_ign = plt.axes([0.12, 0.05, 0.1, 0.075])
-        ax_not = plt.axes([0.59, 0.05, 0.1, 0.075])
-        ax_red = plt.axes([0.7, 0.05, 0.1, 0.075])
-        ax_green = plt.axes([0.81, 0.05, 0.1, 0.075])
-
-        b_not = Button(ax_not, 'Not TL', color='b')
-        b_not.on_clicked(callback.b_not)
-
-        b_ign = Button(ax_ign, 'Ignore', color='w')
-        b_ign.on_clicked(callback.b_ign)
-
-        b_red = Button(ax_red, 'Red', color='r')
-        b_red.on_clicked(callback.b_red)
-
-        b_green = Button(ax_green, 'Green', color='g')
-        b_green.on_clicked(callback.b_green)
-
-        plt.show()
-        img_name = ''
-        new_name = ''
+    i = Index(src_img_dir, des_imag_dir)
+    i.classify()
