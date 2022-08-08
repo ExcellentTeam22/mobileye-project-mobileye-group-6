ff4777bcdac6f537261307ebf3e78d51149470fd
Eliad Karni <EliadK995@Gmail.com>
2022-08-08 23:05:02 +0300

    Cropping finish

--- macros.py
+++ macros.py
@@ -1,25 +1,21 @@
 USER_ID = 0
 NUM_OF_USERS = 4
-# ------------------ CONSTANTS ------------------ #
-RED_COLOR = 0
-GREEN_COLOR = 1
-GRAY_COLOR = 3
-DEFAULT_BASE = "test2"
+# ---------------------------------------------------- CONSTANTS ---------------------------------------------------- #
+RED_COLOR_CODE = 0
+GREEN_COLOR_CODE = 1
+GRAY_COLOR_CODE = 3
 
-SRC_CSV_PATH = "cropped images\\src.csv"
-SRC_CSV_WITH_LABEL_PATH = "cropped images\\src_with_label.csv"
-
-CROPPED_IMAGES_PATH = "cropped images\\data"
-RED_KERNEL_PATH = "kernel trainer/8x8/l2.png"
-GREEN_KERNEL_PATH = "kernel trainer/8x8/l4.png"
-KERNEL_PATH_37 = "kernel trainer/8x8/l2.png"
-KERNEL_PATH_11 = "kernel trainer/8x8/l7.png"
-
-
-GREEN_IMAGES_PATH = "Crop_images\\Green_images"
-RED_IMAGES_PATH = "Crop_images\\Red_images"
-NOT_TRAFFIC_LIGHT = "Crop_images\\Not_traffic_light"
+TESTS_PICS_DIR_PATH = "Boocamp data/leftImg8bit_trainvaltest/leftImg8bit/train"
+CROP_INFO_CSV_PATH = "cropped images\\src.csv"
+LABELS_INFO_PATH = "cropped images\\src_with_label.csv"
+CROPPED_IMAGES_DIR_PATH = "cropped images\\data"
+# -------------------------------------------------- kernels pathes ------------------------------------------------- #
+RED_KERNEL_PATH = "kernels/l2.png"
+GREEN_KERNEL_PATH = "kernels/l4.png"
+KERNEL_PATH_37 = "kernels/l2.png"
+KERNEL_PATH_11 = "kernels/l7.png"
 
 
-CROP_X_PADD = 50
-CROP_Y_PADD = 50
+GREEN_LIGHTS_DIR_PATH = "Crop_images\\Green_images"
+RED_LIGHTS_DIR_PATH = "Crop_images\\Red_images"
+NOT_TRAFFIC_LIGHTS_DIR_PATH = "Crop_images\\Not_traffic_light"
--- part1_api.py
+++ part1_api.py
@@ -11,7 +11,7 @@ try:
     import matplotlib.pyplot as plt
     from typing import List
     from os import walk
-
+    from pathlib import Path
     import pandas as pd
     from scipy import signal as sg, ndimage
     from scipy.ndimage import maximum_filter, convolve
@@ -22,20 +22,6 @@ try:
 except ImportError:
     print("Need to fix the installation")
     raise
-
-def find_tfl_lights(c_image: np.ndarray, **kwargs):
-    """
-    Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement
-    :param c_image: The image itself as np.uint8, shape of (H, W, 3)
-    :param kwargs: Whatever config you want to pass in here
-    :return: 4-tuple of x_red, y_red, x_green, y_green
-    """
-
-    ### WRITE YOUR CODE HERE ###
-    ### USE HELPER FUNCTIONS ###
-
-    return [500, 800, 520], [500, 500, 500], [700, 710], [500, 500]
-
 
 def filter_color(num: int, image: list, list_of_options: list) -> List:
     """
@@ -88,15 +74,14 @@ def design_figure(figure, axarr, image, red_kernel, green_kernel):
     figure.delaxes(axarr[1][1])
 
 
-#def add_image_info(filter_array, dot_color, rect_color, dots_ax, crops_ax, x_padd=50, y_padd=50, x_size=100, y_size=120):
-def add_image_info(filter_array, dot_color, rect_color, dots_ax, crops_ax, x_padd=macros.CROP_X_PADD, y_padd=macros.CROP_Y_PADD):
-
+def add_image_info(filter_array, dot_color, rect_color, dots_ax, crops_ax):
     if filter_array:
         t = np.array(filter_array)
         x, y = t.T
         dots_ax.plot(y, x, dot_color)
         for dot_x, dot_y in zip(x, y):
-            rec = patches.Rectangle((dot_y - y_padd, dot_x - x_padd), 2 * x_padd, 2 * y_padd, linewidth=1, edgecolor=rect_color, facecolor='none')
+            rec = patches.Rectangle((dot_y - 50, dot_x - 50), 2 * 50, 2 * 50, linewidth=1,
+                                    edgecolor=rect_color, facecolor='none')
             crops_ax.add_patch(rec)
 
 
@@ -105,7 +90,7 @@ def add_crops(filter_array, image, image_path, up, down, sides):
     if filter_array:
         t = np.array(filter_array)
         dots_x, dots_y = t.T
-        index = len(next(walk(macros.CROPPED_IMAGES_PATH), (None, None, []))[2])
+        index = len(next(walk(macros.CROPPED_IMAGES_DIR_PATH), (None, None, []))[2])
         for x, y in zip(dots_x, dots_y):
             cropped = image[max(0, x - up):min(np.size(image, 0), x + down), max(0, y - sides):min(np.size(image, 1),
                                                                                                    y + sides)]
@@ -117,16 +102,15 @@ def add_crops(filter_array, image, image_path, up, down, sides):
 
             image_after_convert = np.array(image_after_convert)
 
-            cv2.imwrite(f"{macros.CROPPED_IMAGES_PATH}\\{index}.png", image_after_convert)
-            data["File name"].append(f"{macros.CROPPED_IMAGES_PATH}\\{index}.png")
+            cv2.imwrite(f"{macros.CROPPED_IMAGES_DIR_PATH}\\{index}.png", image_after_convert)
+            data["File name"].append(f"{macros.CROPPED_IMAGES_DIR_PATH}\\{index}.png")
             data["X"].append(f"{x}")
             data["Y"].append(f"{y}")
             data["Source"].append(image_path)
             index += 1
     if data:
         df = pd.DataFrame(data)
-        df.to_csv(macros.SRC_CSV_PATH, mode="a", index=False, header=False)
-
+        df.to_csv(macros.CROP_INFO_CSV_PATH, mode="a", index=False, header=False)
 
 
 def show_3d_filter(image, red_kernel, green_kernel, filter_array_r, filter_array_g, image_path):
@@ -140,57 +124,25 @@ def show_3d_filter(image, red_kernel, green_kernel, filter_array_r, filter_array
     design_figure(f, axarr, image, red_kernel, green_kernel)
     add_image_info(filter_array_r, "r.", "red", axarr[1][0], axarr[2][0])
     add_image_info(filter_array_g, "g.", "green", axarr[1][2], axarr[2][2])
+    plt.show()
 
-### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
-def show_image_and_gt(image_path, image, objs, fig_num=None):
+
+def show_image_and_gt(image_path, image, show=False):
     """
-    1) Create kernel for red and green colors.
-    2) Make convolution between the kernel to the image.
+        1) Create kernels for red and green colors.
+    2) Make convolution between the kernels to the image.
     3) Filter array of red suspicion points of traffic light.
     4) Filter array of green suspicion points of traffic light.
     4) Show 3 image: first- Origin photo, 2) Photo after Kernel 3) Maximum filter Photo with red and green points.
+    :param image_path:
     :param image: matrix of image RGB
-    :param objs:
-    :param fig_num:
+    :param show:
+    :return:
     """
-    # data = np.array(image)
-    #
-    # r_kernel = get_ker(RED_KERNEL_PATH, RED_COLOR)
-    # g_kernel = get_ker(GREEN_KERNEL_PATH, GREEN_COLOR)
-    #
-    # # convert to color:
-    # # image_after_convert = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-    #
-    # # RED
-    # image_after_convert_r = extract_color(RED_COLOR, data)
-    # red_kernel_img = (convolve(image_after_convert_r.astype(float), r_kernel[::-1, ::-1]))
-    # red_kernel_img = normalize_arr(red_kernel_img)
-    # list_of_options = peak_local_max(red_kernel_img, min_distance=80,threshold_abs= 126)
-    # # list_of_options = list(filter(lambda x: (image2[x[0]][x[1]] > 126), list_of_options))
-    # filter_red = filter_color(RED_COLOR, image, list_of_options)
-    #
-    # # GREEN
-    # image_after_convert_g = extract_color(GREEN_COLOR, data)
-    # green_kernel_img = (convolve(image_after_convert_g.astype(float), g_kernel[::-1, ::-1]))
-    # green_kernel_img = normalize_arr(green_kernel_img)
-    # list_of_options = peak_local_max(green_kernel_img, min_distance=80, threshold_abs=100)
-    # filter_green = filter_color(GREEN_COLOR, image, list_of_options)
-    #
-    # show_3d_filter(image, red_kernel_img, green_kernel_img,filter_red, filter_green, image_path)
-    # plt.show()
-    # # labels = set()
-    # # if objs is not None:
-    # #     for o in objs:
-    # #         poly = np.array(o['polygon'])[list(np.arange(len(o['polygon']))) + [0]]
-    # #         plt.plot(poly[:, 0], poly[:, 1], 'r', label=o['label'])
-    # #         labels.add(o['label'])
-    # #     if len(labels) > 1:
-    # #         plt.legend()
-    # ----------
     data = np.array(image)
 
-    kernel_37 = get_ker(macros.KERNEL_PATH_37, macros.GRAY_COLOR)
-    kernel_11 = get_ker(macros.KERNEL_PATH_11, macros.GRAY_COLOR)
+    kernel_37 = get_ker(macros.KERNEL_PATH_37, macros.GRAY_COLOR_CODE)
+    kernel_11 = get_ker(macros.KERNEL_PATH_11, macros.GRAY_COLOR_CODE)
 
     # convert to color:
     image_after_convert = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
@@ -199,15 +151,15 @@ def show_image_and_gt(image_path, image, objs, fig_num=None):
     kernel_img_11 = (convolve(image_after_convert.astype(float), kernel_11[::-1, ::-1]))
     kernel_img_11 = normalize_arr(kernel_img_11)
     list_of_options_11 = peak_local_max(kernel_img_11, min_distance=100, threshold_abs=150)
-    filter_11_r = filter_color(macros.RED_COLOR, image, list_of_options_11)
-    filter_11_g = filter_color(macros.GREEN_COLOR, image, list_of_options_11)
+    filter_11_r = filter_color(macros.RED_COLOR_CODE, image, list_of_options_11)
+    filter_11_g = filter_color(macros.GREEN_COLOR_CODE, image, list_of_options_11)
 
     # 37
     kernel_img_37 = (convolve(image_after_convert.astype(float), kernel_37[::-1, ::-1]))
     kernel_img_37 = normalize_arr(kernel_img_37)
     list_of_options_37 = peak_local_max(kernel_img_37, min_distance=80, threshold_abs=150)
-    filter_37_g = filter_color(macros.GREEN_COLOR, image, list_of_options_37)
-    filter_37_r = filter_color(macros.RED_COLOR, image, list_of_options_37)
+    filter_37_g = filter_color(macros.GREEN_COLOR_CODE, image, list_of_options_37)
+    filter_37_r = filter_color(macros.RED_COLOR_CODE, image, list_of_options_37)
 
     filter_green = filter_11_g + filter_37_g
     filter_red = filter_11_r + filter_37_r
@@ -216,12 +168,9 @@ def show_image_and_gt(image_path, image, objs, fig_num=None):
     add_crops(filter_37_r, image, image_path,sides=15 ,down=81, up=15)
     add_crops(filter_37_g, image, image_path, sides=15, down=15, up=81)
 
+    if show:
+        show_3d_filter(image, kernel_img_37, kernel_img_11, filter_red, filter_green, image_path)
 
-    show_3d_filter(image, kernel_img_37, kernel_img_11, filter_red, filter_green, image_path)
-    #plt.show()
-
-
-# ----------
 
 def get_ker(url, num):
     """
@@ -229,13 +178,13 @@ def get_ker(url, num):
     :param num: What color to convert to 0->red
                                          1->green
                                          3->gray
-    :return: kernel
+    :return: kernels
     """
     image = Image.open(url)
     data = np.array(image)
 
     # convert to color:
-    if num == macros.GRAY_COLOR:
+    if num == macros.GRAY_COLOR_CODE:
         image_after_convert = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
     else:
         image_after_convert = extract_color(num, data)
@@ -262,23 +211,12 @@ def normalize_arr(arr):
     return (255 * (arr - np.min(arr)) / np.ptp(arr)).astype(int)
 
 
-def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
+def test_find_tfl_lights(image_path, fig_num=None, show=False):
     """
     Run the attention code
     """
     image = np.array(Image.open(image_path))
-    if json_path is None:
-        objects = None
-    else:
-        gt_data = json.load(open(json_path))
-        what = ['traffic light']
-        objects = [o for o in gt_data['objects'] if o['label'] in what]
-
-    return show_image_and_gt(image_path, image, objects, fig_num)
-
-    # red_x, red_y, green_x, green_y = find_tfl_lights(image)
-    # plt.plot(red_x, red_y, 'ro', color='r', markersize=4)
-    # plt.plot(green_x, green_y, 'ro', color='g', markersize=4)
+    return show_image_and_gt(image_path, image, show)
 
 
 def main(argv=None):
@@ -288,32 +226,20 @@ def main(argv=None):
     Keep this functionality even after you have all system running, because you sometime want to debug/improve a module
     :param argv: In case you want to programmatically run this
     """
-
+    show = True
     parser = argparse.ArgumentParser("Test TFL attention mechanism")
     parser.add_argument('-i', '--image', type=str, help='Path to an image')
     parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
     parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
     args = parser.parse_args(argv)
-    default_base = "test2"
 
     if args.dir is None:
-        args.dir = default_base
-    flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
-
-    pd_table = pd.DataFrame()  # panda table arg!!!!!
-
+        args.dir = macros.TESTS_PICS_DIR_PATH
+        show = False
+    flist = [x for x in Path(macros.TESTS_PICS_DIR_PATH).rglob('*.png')]
     for i, image in enumerate(flist):
         if i % macros.NUM_OF_USERS == macros.USER_ID:
-            json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
-            if not os.path.exists(json_fn):
-                json_fn = None
-            test_find_tfl_lights(image, json_fn)
-
-    if len(flist):
-        print("You should now see some images, with the ground truth marked on them. Close all to quit.")
-    else:
-        print("Bad configuration?? Didn't find any picture to show")
-    plt.show(block=True)
+            test_find_tfl_lights(str(image), show=show)
 
 
 if __name__ == '__main__':
