901a1afbc5e4cd6abd828c22882a6e17aa528b36
Eliad Karni <EliadK995@Gmail.com>
2022-08-08 14:57:14 +0300

    Tagging interface upgrade

--- macros.py
+++ macros.py
@@ -5,6 +5,7 @@ RED_COLOR = 0
 GREEN_COLOR = 1
 GRAY_COLOR = 3
 DEFAULT_BASE = "test2"
+
 SRC_CSV_PATH = "cropped images\\src.csv"
 CROPPED_IMAGES_PATH = "cropped images\\data"
 RED_KERNEL_PATH = "kernel trainer/8x8/l2.png"
@@ -14,3 +15,6 @@ KERNEL_PATH_11 = "kernel trainer/8x8/l7.png"
 CROP_X_PADD = 50
 CROP_Y_PADD = 50
 DES_IMAG_DIR = 'test2_tag_image'
+DES_IMAG_DIR_RED = 'test2_tag_image'
+DES_IMAG_DIR_GREEN = 'test2_tag_image'
+DES_IMAG_DIR_NON = 'test2_tag_image'
--- part2_tag_image.py
+++ part2_tag_image.py
@@ -1,12 +1,14 @@
 import cv2
 import glob
 from PIL import Image
+import pandas as pd
 import glob
 from termcolor import colored
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import numpy as np
 import macros
+import matplotlib.patches as patches
 
 
 class bcolors:
@@ -32,63 +34,72 @@ class bcolors:
 
 
 class Index(object):
-    def __init__(self, source, dest):
-        self.source= source
-        self.dest =dest
+    def __init__(self, source_path, file_path, x, y):
+        self.source_path = source_path
+        self.source = Image.open(source_path)
+        self.cropped_image_path = file_path
+        self.cropped_image =Image.open(file_path)
         self.img = ''
         self.img_name = ''
         self.new_name = ''
+        self.x = x
+        self.y = y
 
 
     def classify(self):
 
-        for self.filename in glob.glob(f'{self.source}/*.png'):  # assuming gif
-            print(self.filename)
-            self.img = Image.open(self.filename)
-            self.img_name = self.filename
-            f, axarr = plt.subplots(1, 1, sharex=True, sharey=True)
-            axarr.title.set_text('image')
-            axarr.imshow(self.img)
+        print(self.img_name)
+        f, axarr = plt.subplots(1, 2)
+        axarr[0].title.set_text('image')
+        axarr[1].title.set_text('crop')
+        axarr[0].imshow(self.source)
+        axarr[1].imshow(self.cropped_image)
+
+        ax_ign = plt.axes([0.12, 0.05, 0.1, 0.075])
+        ax_not = plt.axes([0.59, 0.05, 0.1, 0.075])
+        ax_red = plt.axes([0.7, 0.05, 0.1, 0.075])
+        ax_green = plt.axes([0.81, 0.05, 0.1, 0.075])
+
+        b_not = Button(ax_not, 'Not TL', color='b')
+        b_not.on_clicked(self.b_not)
+
+        b_ign = Button(ax_ign, 'Ignore', color='w')
+        b_ign.on_clicked(self.b_ign)
+
+        b_red = Button(ax_red, 'Red', color='r')
+        b_red.on_clicked(self.b_red)
+
+        b_green = Button(ax_green, 'Green', color='g')
+        b_green.on_clicked(self.b_green)
+
+        rect = patches.Rectangle((self.y - 50, self.x - 50), 100, 100, linewidth=1, edgecolor='r', facecolor='none')
+
+        # Add the patch to the Axes
+        axarr[0].add_patch(rect)
 
 
-            ax_ign = plt.axes([0.12, 0.05, 0.1, 0.075])
-            ax_not = plt.axes([0.59, 0.05, 0.1, 0.075])
-            ax_red = plt.axes([0.7, 0.05, 0.1, 0.075])
-            ax_green = plt.axes([0.81, 0.05, 0.1, 0.075])
+        plt.show()
+        self.img_name = ''
+        self.new_name = ''
 
-            b_not = Button(ax_not, 'Not TL', color='b')
-            b_not.on_clicked(self.b_not)
-
-            b_ign = Button(ax_ign, 'Ignore', color='w')
-            b_ign.on_clicked(self.b_ign)
-
-            b_red = Button(ax_red, 'Red', color='r')
-            b_red.on_clicked(self.b_red)
-
-            b_green = Button(ax_green, 'Green', color='g')
-            b_green.on_clicked(self.b_green)
-
-            plt.show()
-            self.img_name = ''
-            self.new_name = ''
 
     def b_green(self, event):
-        img_name2 = self.img_name.replace(macros.DEFAULT_BASE, macros.DES_IMAG_DIR)
-        self.new_name = img_name2.replace(".png", "_G.png")
+        self.new_name = self.img_name.replace(macros.DEFAULT_BASE, macros.DES_IMAG_DIR_GREEN)
+        # self.new_name = img_name2.replace(".png", "_G.png")
         self.save_image(self.new_name)
         print(f"{bcolors.Green}{self.new_name}{bcolors.ENDC}")
 
 
     def b_red(self, event):
-        img_name2 = self.img_name.replace(self.source,self.dest)
-        self.new_name = img_name2.replace(".png", "_R.png")
+        self.new_name = self.img_name.replace(macros.DEFAULT_BASE, macros.DES_IMAG_DIR_RED)
+        # self.new_name = img_name2.replace(".png", "_R.png")
         self.save_image(self.new_name)
         print(f"{bcolors.Red}{self.new_name}{bcolors.ENDC}")
 
 
     def b_not(self, event):
-        img_name2 = self.img_name.replace(self.source,self.dest)
-        self.new_name = img_name2.replace(".png", "_N.png")
+        self.new_name = self.img_name.replace(macros.DEFAULT_BASE, macros.DES_IMAG_DIR_NON)
+        # self.new_name = img_name2.replace(".png", "_N.png")
         self.save_image(self.new_name)
         print(f"{bcolors.Blue}{self.new_name}{bcolors.ENDC}")
 
@@ -106,5 +117,11 @@ class Index(object):
         plt.close()
 
 if __name__ == '__main__':
-    i = Index(macros.DEFAULT_BASE, macros.DES_IMAG_DIR)
-    i.classify()
+    for i, row in pd.read_csv(macros.SRC_CSV_PATH).iterrows():
+        print(row["Source"])
+        source_path = row["Source"]
+        file_path = row["File name"]
+        x = row["X"]
+        y = row["Y"]
+        i = Index(source_path, file_path, x, y)
+        i.classify()
